ae27909e26bd06f6d994523c4d99fe6e6ed12c92
GitHubSolik <sbereznichenko@gmail.com>
2023-08-29 15:36:45 +0900

    my second commit

---
+++ advertisements/app_advertisements/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from .views import index, top_sellers, advertisement_post, register
+
+urlpatterns = [
+    path('', index, name='main-page'),
+    path('top-sellers/', top_sellers, name='top-sellers'),
+    path('advertisement-post/', advertisement_post, name='adv-post'),
+    path('register/', register, name='register'),
+]
